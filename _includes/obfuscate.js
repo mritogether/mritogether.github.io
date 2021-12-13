3019164ce6db6a66fc308e646cebee5d7bfe7db6
fsantini <francesco.santini@gmail.com>
2021-12-13 10:25:06 +0100

    js fix

--- _includes/obfuscate.js
+++ _includes/obfuscate.js
@@ -1,7 +1,7 @@
 function obfuscate(link, name, server)
 {
 	document.write('<a href=\"mail');
-        document.write('to:' + name + '@' + server'\">');
+        document.write('to:' + name + '@' + server + '\">');
         document.write(link);
         document.write('</a>');
 }
