d5844f55e2556738e1e606e3eece2aa78084d0d3
shahdloo <shahdloo@gmail.com>
2022-06-25 22:14:37 +0100

    added twitter login

--- assets/js/init-firebase.js
+++ assets/js/init-firebase.js
@@ -2,8 +2,8 @@
 
 var uiConfig = {
   signInSuccessUrl: '/',
-  signInOptions: [firebase.auth.GithubAuthProvider.PROVIDER_ID], 
-    // firebase.auth.GoogleAuthProvider.PROVIDER_ID, 
+  signInOptions: [firebase.auth.GithubAuthProvider.PROVIDER_ID, 
+    firebase.auth.TwitterAuthProvider.PROVIDER_ID], 
     // firebase.auth.EmailAuthProvider.PROVIDER_ID],
   signInFlow: 'popup',
   // data-cancel_on_tap_outside="false",
